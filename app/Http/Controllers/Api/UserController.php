684062c3ef51f8d1939c5c0e7ce50cbbd6f75e8f
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-09-19 22:18:19 +0300

    Single Responsibility Principle

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,4 +24,14 @@ class UserController extends Controller
         $this->emailService->sendWelcomeEmail($user);
         return response()->json(['message' => 'User registered']);
     }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('authToken')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
 }
